144efb642d905b52e90176703d9fa1984300beca
LwamB <Lwamberihu0@gmail.com>
2023-06-03 18:24:04 +0200

    Add project files.

---
+++ MvcMovie/DAL/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcMovie.DAL
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(int id);
+        void DeleteAll();
+        void Save();
+    }
+}
